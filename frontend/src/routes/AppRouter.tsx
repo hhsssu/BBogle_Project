90fcfdb9ded841efeb29805ac8744a08e7087810
이지혜 <hyeee19@naver.com>
2024-10-30 12:44:57 +0900

    [CHORE] : 들여쓰기 변경

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,12 +1,17 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 import OnboardingPage from '../pages/OnboardingPage';
+import MainPage from '../pages/MainPage';
 
 function AppRouter() {
   return (
     <Router>
       <Routes>
         // 온보딩 페이지
-        <Route path='/' element={<OnboardingPage></OnboardingPage>}></Route>
+        <Route path='/' element={<OnboardingPage />}></Route>
+
+        // 메인 페이지
+        <Route path='/main' element={<MainPage />}></Route>
       </Routes>
     </Router>
   )

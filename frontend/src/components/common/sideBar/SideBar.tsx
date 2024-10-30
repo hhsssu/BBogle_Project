3c579076b06c68a28fde5d2ca2a3e64e87e18c59
이지혜 <hyeee19@naver.com>
2024-10-30 14:39:59 +0900

    [FEAT] : SideBar 펼치기/닫기 기능 구현

--- frontend/src/components/common/sideBar/SideBar.tsx
+++ frontend/src/components/common/sideBar/SideBar.tsx
@@ -1,10 +1,16 @@
 import style from './SideBar.module.css';
 import MainIcon from '../../../assets/image/icon/main.svg';
+import useSideBarStore from '../../../store/useSideBarStore';
 
 function SideBar () {
+  const {isOpen, toggleSideBar, activeTab, setActiveTab} = useSideBarStore();
+
   return (
-    <div className={style['sidebar-container']}>
-      <img src={MainIcon} alt="Main Icon" />
+    <div className={`${style['sidebar-container']} ${isOpen ? style.open : style.closed}`}>
+      <button className={style.toggleButton} onClick={toggleSideBar}>닫기</button>
+      <div className={style.content}>
+        <img src={MainIcon} alt="Main Icon" />
+      </div>
     </div>
   )
 }
--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 
 import OnboardingPage from '../pages/OnboardingPage';
 import MainPage from '../pages/MainPage';
@@ -6,17 +6,15 @@ import Main from '../components/main/Main';
 
 function AppRouter() {
   return (
-    <Router>
-      <Routes>
-        // 온보딩 페이지
-        <Route path='/' element={<OnboardingPage />}></Route>
+    <Routes>
+      // 온보딩 페이지
+      <Route path='/' element={<OnboardingPage />}></Route>
 
-        // 메인 페이지
-        <Route path='/main' element={<MainPage />}>
-          <Route index element={<Main />} />
-        </Route>
-      </Routes>
-    </Router>
+      // 메인 페이지
+      <Route path='/main' element={<MainPage />}>
+        <Route index element={<Main />} />
+      </Route>
+    </Routes>
   )
 }
 

6ea7b91e88b17e12c518239471b4357735fdb19f
이지혜 <hyeee19@naver.com>
2024-10-30 13:30:16 +0900

    [DESIGN] : #6 Main Icon 수정

--- frontend/src/components/common/sideBar/SideBar.tsx
+++ frontend/src/components/common/sideBar/SideBar.tsx
@@ -1,10 +1,11 @@
+import style from './SideBar.module.css';
 import MainIcon from '../../../assets/image/icon/main.svg';
 
 function SideBar () {
   return (
-    <>
+    <div className={style['sidebar-container']}>
       <img src={MainIcon} alt="Main Icon" />
-    </>
+    </div>
   )
 }
 

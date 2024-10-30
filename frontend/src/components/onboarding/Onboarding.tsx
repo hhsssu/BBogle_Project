3d682876e43a9d83ba903a71e51fbec472dae535
이지혜 <hyeee19@naver.com>
2024-10-30 10:01:39 +0900

    [DESIGN] : 온보딩 css module 추가

--- frontend/src/components/onboarding/Onboarding.tsx
+++ frontend/src/components/onboarding/Onboarding.tsx
@@ -1,10 +1,11 @@
 import KakaoLogin from '../../assets/image/KakaoLogin.png';
+import style from './Onboarding.module.css';
 
 function Onboarding() {
   return (
     <>
       <div>매일의 기록으로, 자소서를 더 쉽게</div>
-      <img className='kakao' src={KakaoLogin} alt="" />
+      <img className={style['kakao-login']} src={KakaoLogin} alt="Kakao Login" />
     </>
   )
 }

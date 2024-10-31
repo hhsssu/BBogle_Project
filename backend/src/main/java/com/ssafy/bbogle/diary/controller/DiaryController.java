e5091ca2d8e485e1d43a74f012d1442bea640642
김선하 <ksh219805@gmail.com>
2024-10-31 16:49:31 +0900

    [FEAT] : 개발일지 작성 API

--- backend/src/main/java/com/ssafy/bbogle/diary/controller/DiaryController.java
+++ backend/src/main/java/com/ssafy/bbogle/diary/controller/DiaryController.java
@@ -1,5 +1,6 @@
 package com.ssafy.bbogle.diary.controller;
 
+import com.ssafy.bbogle.diary.dto.request.DiaryCreateResponse;
 import com.ssafy.bbogle.diary.dto.response.DiaryListResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -9,6 +10,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "DiaryController", description = "개발일지 컨트롤러")
 public class DiaryController {
 
-    @Operation(summary = "개발일지 목록 조회",
-        description = "특정 프로젝트의 개발일지 목록 조회")
+    @Operation(summary = "특정 프로젝트의 개발일지 목록 조회")
     @Parameters(value = {
         @Parameter(name = "projectId", description = "프로젝트 ID", in = ParameterIn.PATH)
     })
@@ -26,5 +28,16 @@ public class DiaryController {
     public ResponseEntity<DiaryListResponse> getAllDiaries(@PathVariable("projectId") String projectId){
         return null;
     }
+
+    @Operation(summary = "개발일지 등록")
+    @Parameters(value = {
+        @Parameter(name = "projectId", description = "프로젝트 ID", in = ParameterIn.PATH)
+    })
+    @PostMapping("/{projectId}/diaries")
+    public ResponseEntity<String> createDiary(@PathVariable("projectId") String projectId,
+        @RequestBody DiaryCreateResponse request){
+        return null;
+    }
+
 
 }
---
+++ backend/src/main/java/com/ssafy/bbogle/diary/dto/request/DiaryCreateResponse.java
@@ -0,0 +1,19 @@
+package com.ssafy.bbogle.diary.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@Schema(description = "개발일지 작성 요청 DTO")
+public class DiaryCreateResponse {
+
+    @Schema(description = "답변")
+    private List<String> answers;
+
+    @Schema(description = "첨부 이미지")
+    private List<String> images;
+
+}

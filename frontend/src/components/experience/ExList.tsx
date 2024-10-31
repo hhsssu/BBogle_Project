81002e2c6740a55dc8c297ea5b3c437d69679db1
hhsssu <hhssuu14@naver.com>
2024-10-31 14:43:11 +0900

    [FEAT] : 경험 전체 목록 퍼블리싱 및 페이지 연결

--- frontend/src/components/experience/ExList.tsx
+++ frontend/src/components/experience/ExList.tsx
@@ -1,13 +1,14 @@
 import ExCard from './ExCard';
 import ExStyles from './Experience.module.css';
 
+import searchIcon from '../../assets/image/icon/search.svg';
 import emptyFolder from '../../assets/image/icon/emptyFolder.svg';
 import { useNavigate } from 'react-router-dom';
 
 function ExList() {
   const nav = useNavigate();
-  const navDetail = () => {
-    nav('detail');
+  const navExCreate = () => {
+    nav('create');
   };
 
   // 더미 데이터 예시
@@ -157,27 +158,41 @@ function ExList() {
     },
   ];
   return (
-    <section className={ExStyles.list}>
-      {exCardList.length > 0 ? (
-        exCardList.map((exCard, index) => (
-          <div key={index} onClick={navDetail}>
-            <ExCard
-              exId={exCard.exId}
-              title={exCard.title}
-              startDate={new Date(exCard.startDate)}
-              endDate={new Date(exCard.endDate)}
-              tags={exCard.tags}
-            />
+    <>
+      <section className={ExStyles.between}>
+        <div className={ExStyles.title}>나의 경험</div>
+        <button className={ExStyles.btn} onClick={navExCreate}>
+          + 경험 추가
+        </button>
+      </section>
+
+      {/* 검색 버튼 - 클릭 시 검색 모달 OPEN */}
+      <button className={ExStyles.search}>
+        <img src={searchIcon} alt="검색" />
+        <span>키워드, 내용으로 검색</span>
+      </button>
+
+      <section className={ExStyles.list}>
+        {exCardList.length > 0 ? (
+          exCardList.map((exCard, index) => (
+            <div key={index}>
+              <ExCard
+                exId={exCard.exId}
+                title={exCard.title}
+                startDate={new Date(exCard.startDate)}
+                endDate={new Date(exCard.endDate)}
+                tags={exCard.tags}
+              />
+            </div>
+          ))
+        ) : (
+          <div className={ExStyles.nothing}>
+            <img src={emptyFolder} alt="비어있는 경험" />
+            <p>만들어진 경험이 없어요.</p>
+            <p className={ExStyles.bold}>경험을 추가해 주세요!</p>
           </div>
-        ))
-      ) : (
-        <div className={ExStyles.nothing}>
-          <img src={emptyFolder} alt="비어있는 경험" />
-          <p>만들어진 경험이 없어요.</p>
-          <p className={ExStyles.bold}>경험을 추가해 주세요!</p>
-        </div>
-      )}
-      {/* <ExCard
+        )}
+        {/* <ExCard
         title="소셜 로그인 구현"
         startDate={new Date('2024-01-01')}
         endDate={new Date('2024-02-01')}
@@ -185,9 +200,10 @@ function ExList() {
           { type: 0, name: '기술태그' },
           { type: 0, name: '기술태그' },
           { type: 1, name: '인성태그' },
-        ]}
-      /> */}
-    </section>
+          ]}
+          /> */}
+      </section>
+    </>
   );
 }
 

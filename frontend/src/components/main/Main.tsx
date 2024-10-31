4f5ec2901bedb55d08e269b3f1f7d64433cd43a1
이지혜 <hyeee19@naver.com>
2024-10-31 13:12:43 +0900

    [CHORE] : lottie react 라이브러리 추가

--- frontend/src/components/main/Main.tsx
+++ frontend/src/components/main/Main.tsx
@@ -1,6 +1,33 @@
+import style from './Main.module.css';
+import Lottie from 'lottie-react';
+import animationData from '../../assets/lottie/Welcome.json';
+
 function Main() {
+  // TODO : 임시데이터
+  const userName = '홍길동';
+
   return (
-    <div>main</div>
+    <div className={style.container}>
+      <div className={style.header}>
+        <Lottie
+          animationData={animationData}
+          loop={false}
+          autoplay={true}
+          className={style.welcome} 
+        />
+        <div className={style.greeting}>안녕하세요, {userName}님!</div>
+      </div>
+
+      <div className={style.diary}>
+        <div className={style.description}>
+          <div className={style.mainDescription}>오늘의 성장 기록을 남겨보세요 :-)</div>
+          <div className={style.subDescription}>개발한 프로젝트를 선택해주세요 !</div>
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    </div>
   );
 }
 

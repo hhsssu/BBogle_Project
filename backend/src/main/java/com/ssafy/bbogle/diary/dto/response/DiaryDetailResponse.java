5e7d9b16163af1df367c92c5655f9ad813338b5a
김선하 <ksh219805@gmail.com>
2024-10-31 20:23:47 +0900

    [FEAT] : 개발일지 상세 조회 API, DTO

--- backend/src/main/java/com/ssafy/bbogle/diary/dto/response/DiaryDetailResponse.java
+++ backend/src/main/java/com/ssafy/bbogle/diary/dto/response/DiaryDetailResponse.java
@@ -1,5 +1,19 @@
 package com.ssafy.bbogle.diary.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@Schema(description = "개발일지 상세 조회 응답 DTO")
 public class DiaryDetailResponse {
+
+    @Schema(description = "답변 리스트")
+    private List<String> answers;
+
+    @Schema(description = "첨부 이미지 리스트")
+    private List<String> images;
 
 }

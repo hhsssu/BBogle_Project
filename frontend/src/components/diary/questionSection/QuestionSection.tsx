22597c957fd9d4cebc040a827d04b2495d07b78d
Taehankk <162579158+Taehankk@users.noreply.github.com>
2024-11-04 00:15:55 +0900

    [FEAT] : 개발일지 작성 페이지 - 이미지 업로드 후 미리보기 기능

--- frontend/src/components/diary/questionSection/QuestionSection.tsx
+++ frontend/src/components/diary/questionSection/QuestionSection.tsx
@@ -1,5 +1,9 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+
 import style from './QuestionSection.module.css';
+
+import ImageUpload from '../../../assets/image/icon/ImageUpload.svg';
+import Close from '../../../assets/image/icon/Close.svg';
 
 interface Props {
   index: number;
@@ -20,8 +24,37 @@ function QuestionSection({
 }: Props) {
   const [textValue, setTextValue] = useState('');
 
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const [files, setFiles] = useState<string[]>([]);
+
   const handleText = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setTextValue(event.target.value);
+  };
+
+  const handleImgUploadClick = () => {
+    if (fileInputRef.current && files.length < 3) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const isImage = file.type.startsWith('image/');
+      console.log(file.type);
+      console.log(isImage);
+      if (!isImage) {
+        alert('이미지 파일만 업로드 가능합니다 !');
+        return;
+      }
+      const newImageUrl = URL.createObjectURL(file); // 새 이미지 URL 생성
+      setFiles((prevImgArr) => [...prevImgArr, newImageUrl]); // 이미지 상태 업데이트
+    }
+  };
+
+  const handleImageDelete = (deleteIndex: number) => {
+    setFiles((prevFiles) => prevFiles.filter((_, idx) => idx !== deleteIndex));
   };
 
   return (
@@ -36,7 +69,47 @@ function QuestionSection({
 
       <p className={style.description}>{description}</p>
       {isFileType ? (
-        <input className={style.textArea} type="file"></input>
+        <div>
+          <div
+            className={`${style.textArea} ${style.imgInput}`}
+            onClick={handleImgUploadClick}
+          >
+            {files.length > 0 ? (
+              <div className={style.imgInput}>
+                {files.map((file, index) => (
+                  <div key={index} className={style.imgExBlock}>
+                    <img
+                      key={index + 'img'}
+                      className={style.imgEx}
+                      src={file}
+                    ></img>
+                    <img
+                      key={index + 'Close'}
+                      className={style.close}
+                      src={Close}
+                      alt="삭제"
+                      onClick={() => handleImageDelete(index)}
+                    />
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div className={style.imgInput}>
+                <img src={ImageUpload} alt="이미지 업로드" />
+                <div>
+                  <p>이미지를 업로드 해주세요.</p>
+                  <p>(.jpg, .jpeg, .png, .svg)</p>
+                </div>
+              </div>
+            )}
+          </div>
+          <input
+            ref={fileInputRef}
+            className={style.hiddenInput}
+            type="file"
+            onChange={handleImageUpload}
+          ></input>
+        </div>
       ) : (
         <div className={style.textBlock}>
           <textarea

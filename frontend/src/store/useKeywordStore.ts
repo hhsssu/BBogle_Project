0969da3938b0c67120af29688cef442e7bcc7cf4
hhsssu <hhssuu14@naver.com>
2024-11-04 17:54:52 +0900

    [FEAT] : 경험 관련 API Store 생성

---
+++ frontend/src/store/useKeywordStore.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { create } from 'zustand';
+
+const API_LINK = import.meta.env.VITE_API_URL;
+
+export interface Keyword {
+  type: number;
+  name: string;
+}
+
+interface KeywordState {
+  keywords: Keyword[];
+  fetchKeywords: () => Promise<void>;
+}
+
+const useKeywordStore = create<KeywordState>((set) => ({
+  keywords: [],
+
+  // 키워드 리스트 가져오기
+  fetchKeywords: async () => {
+    try {
+      // 키워드 불러오기 API 주소 재확인
+      const response = await axios.get(`${API_LINK}/activites/keywords`);
+      set({ keywords: Array.isArray(response.data) ? response.data : [] });
+    } catch (error) {
+      console.error('키워드 데이터를 가져오는 데 실패했습니다: ', error);
+    }
+  },
+}));
+
+export default useKeywordStore;

fe48e5fa9abc442a6cc534b3d2d484c9da7bfe31
hhsssu <hhssuu14@naver.com>
2024-11-06 14:07:08 +0900

    [REFACTOR] : 경험 키워드 목록 불러오는 부분 Keyword -> ActivityKeyword로 변경

--- frontend/src/store/useActivityKeywordStore.ts
+++ frontend/src/store/useActivityKeywordStore.ts
@@ -3,30 +3,32 @@ import { create } from 'zustand';
 
 const API_LINK = import.meta.env.VITE_API_URL;
 
-export interface Keyword {
+export interface ActivityKeyword {
   id: number;
   type: boolean;
   name: string;
 }
 
-interface KeywordState {
-  keywords: Keyword[];
-  fetchKeywords: () => Promise<void>;
+interface ActivityKeywordState {
+  activityKeywords: ActivityKeyword[];
+  fetchActivityKeywords: () => Promise<void>;
 }
 
-const useKeywordStore = create<KeywordState>((set) => ({
-  keywords: [],
+const useActivityKeywordStore = create<ActivityKeywordState>((set) => ({
+  activityKeywords: [],
 
   // 키워드 리스트 가져오기
-  fetchKeywords: async () => {
+  fetchActivityKeywords: async () => {
     try {
       // 키워드 불러오기 API 주소 재확인
       const response = await axios.get(`${API_LINK}/activites/keywords`);
-      set({ keywords: Array.isArray(response.data) ? response.data : [] });
+      set({
+        activityKeywords: Array.isArray(response.data) ? response.data : [],
+      });
     } catch (error) {
       console.error('키워드 데이터를 가져오는 데 실패했습니다: ', error);
     }
   },
 }));
 
-export default useKeywordStore;
+export default useActivityKeywordStore;

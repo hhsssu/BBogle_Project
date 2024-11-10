4bbd3ae6c8b7608ed4c0676607e759431b4a3c3e
kimseonha <ksh219805@gmail.com>
2024-11-10 20:21:56 +0900

    [FEAT] : user 관련api 구현

--- backend/src/main/java/com/ssafy/bbogle/user/service/UserService.java
+++ backend/src/main/java/com/ssafy/bbogle/user/service/UserService.java
@@ -1,5 +1,7 @@
 package com.ssafy.bbogle.user.service;
 
+import com.ssafy.bbogle.user.dto.request.UpdateNicknameRequest;
+import com.ssafy.bbogle.user.dto.response.UserInfoResponse;
 import com.ssafy.bbogle.user.dto.response.UserNicknameResponse;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -8,4 +10,8 @@ public interface UserService {
     UserNicknameResponse getUserNickname();
 
     void logout(HttpServletResponse response);
+
+    UserInfoResponse getUserInfo();
+
+    void updateNickname(UpdateNicknameRequest request);
 }

ab65154f0a46f438230e28d10fe74c782bb7276e
박예본 <goodyebon123@naver.com>
2024-11-11 00:13:47 +0900

    Update fast_api/app/main.py

--- fast_api/app/main.py
+++ fast_api/app/main.py
@@ -96,3 +96,8 @@ async def summarize_devlog(
     except Exception as e:
         logger.error(f"제목 생성중 에러 ㅠ : {e}")
         raise
+
+# 서버 실행
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0', port=8000)

dadd52b4e21f47e7cb909dc104a8036743970353
이지혜 <hyeee19@naver.com>
2024-11-12 12:34:50 +0900

    [CHORE] : 온보딩 페이지외 로그인하지 않은 사용자 접근 제한

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -18,12 +18,12 @@ function AppRouter() {
         <Route path="/" element={<MainPage />}>
           <Route index element={<Main />} />
         </Route>
+        {/* 프로젝트 페이지 */}
+        <Route path="project/*" element={<ProjectPage></ProjectPage>} />
+        <Route path="activity/*" element={<ActivityPage />} />
+        {/* 마이 페이지 */}
+        <Route path="my" element={<MyPage />}></Route>
       </Route>
-      {/* 프로젝트 페이지 */}
-      <Route path="project/*" element={<ProjectPage></ProjectPage>} />
-      <Route path="activity/*" element={<ActivityPage />} />
-      {/* 마이 페이지 */}
-      <Route path="my" element={<MyPage />}></Route>
     </Routes>
   );
 }

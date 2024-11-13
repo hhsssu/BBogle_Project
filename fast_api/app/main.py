789a4796e0ad81f9342f26b0fe74f7dddfcb9c00
박예본 <goodyebon123@naver.com>
2024-11-13 10:14:41 +0900

    Update fast_api/app/main.py, fast_api/Jenkinsfile

--- fast_api/app/main.py
+++ fast_api/app/main.py
@@ -10,8 +10,7 @@ import logging
 # 서버 실행
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000)
-
+    uvicorn.run("main:app", host='0.0.0.0', port=8000)
 
 # 로깅 설정
 logging.basicConfig(

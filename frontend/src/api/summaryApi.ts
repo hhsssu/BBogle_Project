c82438cebefcc612cf52769b8148c129daa2e4bd
hhsssu <hhssuu14@naver.com>
2024-11-13 09:48:25 +0900

    [FEAT] : 회고 관련 API 구현

--- frontend/src/api/summaryApi.ts
+++ frontend/src/api/summaryApi.ts
@@ -1,13 +1,46 @@
 import axiosInstance from './axiosInstance';
 import useProjectStore from '../store/useProjectStore';
 
+// 프로젝트 ID 가져오기
+const projectId = useProjectStore((state) => state.project.projectId);
+
 // 회고 가져오기
 export const fetchSummaryInfo = async () => {
   try {
-    const projectId = useProjectStore((state) => state.project.projectId);
     const response = await axiosInstance.get(`/projects/${projectId}/summary`);
     return response.data;
   } catch (error) {
     console.error('프로젝트 회고 가져오기 실패: ', error);
+  }
+};
+
+// 회고 수동 생성
+export const createSummary = async (content: string) => {
+  try {
+    const response = await axiosInstance.post(
+      `/projects/${projectId}/summary`,
+      {
+        content: content,
+      },
+    );
+    console.log('회고 수동 생성 성공: ', response.data);
+  } catch (error) {
+    console.error('프로젝트 회고 수동 생성 실패: ', error);
+  }
+};
+
+// TODO 회고 ID?
+// 회고 수정
+export const updateSummary = async (content: string) => {
+  try {
+    // const response = await axiosInstance.patch(
+    //   `/projects/${projectId}/summary/${summaryId}`,
+    //   {
+    //     content: content,
+    //   },
+    // );
+    // console.log('회고 수정 성공: ', response.data);
+  } catch (error) {
+    console.error('프로젝트 회고 수정 실패: ', error);
   }
 };

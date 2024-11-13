1d76b5d21ed6e7750f11fcd92086c6fa72081123
hhsssu <hhssuu14@naver.com>
2024-11-13 09:48:13 +0900

    [FEAT] : 경험 관련 API 구현

--- frontend/src/api/activityApi.ts
+++ frontend/src/api/activityApi.ts
@@ -1,1 +1,57 @@
+import { Activity } from './../store/useActivityStore';
 import axiosInstance from './axiosInstance';
+import useActivityStore from '../store/useActivityStore';
+
+const activityId = useActivityStore((state) => state.activity.activityId);
+
+// 경험 수동 생성
+export const createActivity = async (activity: Activity) => {
+  try {
+    const response = await axiosInstance.post('/activiies', {
+      title: activity.title,
+      content: activity.content,
+      startDate: activity.startDate,
+      endDate: activity.endDate,
+      keywords: activity.keywords,
+      projectId: activity.projectId,
+    });
+    console.log('경험 수동 생성 성공: ', response.data);
+  } catch (error) {
+    console.error('경험 수동 생성 실패: ', error);
+  }
+};
+
+// TODO 경험 수정
+export const updateActivity = async () => {
+  try {
+  } catch (error) {
+    console.error('경험 수정 실패: ', error);
+  }
+};
+
+// 경험 목록 조회
+export const fetchActivities = async (
+  word: string | null = null,
+  keywords: number[] = [],
+  projects: number[] = [],
+) => {
+  try {
+    const response = await axiosInstance.post('/activities/search', {
+      word: word,
+      keywords: keywords,
+      projects: projects,
+    });
+    return response.data;
+  } catch (error) {
+    console.error('경험 목록 조회 실패: ', error);
+  }
+};
+
+export const fetchActivityById = async () => {
+  try {
+    const response = await axiosInstance.get(`/activities/${activityId}`);
+    return response.data;
+  } catch (error) {
+    console.error('경험 상세 조회 실패: ', error);
+  }
+};

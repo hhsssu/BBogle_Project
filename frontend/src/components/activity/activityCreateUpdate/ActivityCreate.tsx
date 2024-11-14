1f8be996dd757881c2770d2059cc1c91854fa83f
hhsssu <hhssuu14@naver.com>
2024-11-14 20:13:00 +0900

    [FEAT] : 경험 수동 생성 / 수정 구현 완료

--- frontend/src/components/activity/activityCreateUpdate/ActivityCreate.tsx
+++ frontend/src/components/activity/activityCreateUpdate/ActivityCreate.tsx
@@ -1,61 +1,94 @@
 import { useNavigate } from 'react-router-dom';
+import useActivityStore from '../../../store/useActivityStore';
 
 import ActivityForm from './ActivityForm';
+import Modal from '../../common/modal/Modal';
 
 import ActivityStyles from '../Activity.module.css';
 import ActivityCreateStyles from './ActivityCreate.module.css';
 
+import AlertTriangle from '../../../assets/image/icon/AlertTriangle.svg';
 import BackIcon from '../../../assets/image/icon/Back.svg';
-import useActivityStore from '../../../store/useActivityStore';
-import { useState } from 'react';
 
+import { useEffect, useState } from 'react';
+
+// 경험 수동 생성 컴포넌트
 function ActivityCreate() {
   const nav = useNavigate();
+  const activity = useActivityStore((state) => state.activity);
   const createActivity = useActivityStore((state) => state.createActivity);
+  const resetActivity = useActivityStore((state) => state.resetActivity);
 
-  // 폼 상태 관리
-  const [formData, setFormData] = useState<{
-    title: string;
-    content: string;
-    startDate: Date;
-    endDate: Date;
-    projectId: number | undefined;
-    keywords: { id: number; type: boolean; name: string }[];
-  }>({
-    title: '',
-    content: '',
-    startDate: new Date(),
-    endDate: new Date(),
-    projectId: undefined,
-    keywords: [],
-  });
+  // 폼 오류 설정하기
+  const {
+    titleError,
+    setTitleError,
+    contentError,
+    setContentError,
+    termError,
+    setTermError,
+    errMsgOn,
+    setErrMsgOn,
+  } = useActivityStore();
 
-  // 돌아가기
+  const [isBackModalOpen, setBackModalOpen] = useState(false);
+  const [isCreateModalOpen, setCreateModalOpen] = useState(false);
+
+  // ✅ 돌아가기
   const handleGoBack = () => {
-    nav(-1);
+    nav('/activity');
   };
 
-  // 폼 제출 로직
-  const handleFormSubmit = () => {
-    event?.preventDefault();
-    createActivity(formData);
+  const handleBackModal = () => {
+    setBackModalOpen(!isBackModalOpen);
   };
 
-  // 폼 데이터 업데이트
-  const handleFormChange = (updatedData: {
-    title: string;
-    content: string;
-    startDate: Date;
-    endDate: Date;
-    projectId: number | undefined;
-    keywords: { id: number; type: boolean; name: string }[];
-  }) => {
-    setFormData(updatedData);
+  const handleCreateModal = () => {
+    setCreateModalOpen(!isCreateModalOpen);
   };
+
+  // ✅ 폼 제출 로직
+  const submitForm = () => {
+    // 1. 빈 값 오류 확인
+    if (activity.title === '') {
+      setTitleError(true);
+      setErrMsgOn(true);
+      return;
+    }
+
+    if (activity.content === '') {
+      setContentError(true);
+      setErrMsgOn(true);
+      return;
+    }
+
+    // 2. 날짜 오류 확인
+    if (activity.startDate > activity.endDate) {
+      setTermError(true);
+      setErrMsgOn(true);
+      return;
+    }
+
+    setCreateModalOpen(true);
+  };
+
+  // 경험 생성
+  const handleCreateActivity = async () => {
+    setCreateModalOpen(!isCreateModalOpen);
+    createActivity(activity);
+  };
+
+  useEffect(() => {
+    resetActivity();
+    setTitleError(false);
+    setContentError(true);
+    setTermError(false);
+    setErrMsgOn(false);
+  }, []);
 
   return (
     <>
-      <div className={ActivityStyles.backBtn} onClick={handleGoBack}>
+      <div className={ActivityStyles.backBtn} onClick={handleBackModal}>
         <img src={BackIcon} alt="돌아가기" />
         돌아가기
       </div>
@@ -66,17 +99,49 @@ function ActivityCreate() {
         >
           경험 작성
         </div>
-        <button
-          className={`${ActivityStyles.regist} ${ActivityCreateStyles.regist}`}
-          onClick={handleFormSubmit}
-        >
-          등록하기
-        </button>
       </section>
       <ActivityForm
-        onChange={handleFormChange}
-        onSubmit={handleFormSubmit}
-        initialValues={formData}
+        title={activity.title}
+        content={activity.content}
+        startDate={activity.startDate}
+        endDate={activity.endDate}
+        projectId={activity.projectId}
+        keywords={activity.keywords.map((keyword) => keyword.id)}
+      />
+      <button
+        className={`${ActivityStyles.btn} ${ActivityCreateStyles.regist} ${(titleError || contentError || termError) && errMsgOn && ActivityCreateStyles.failBtn}`}
+        onClick={submitForm}
+      >
+        등록하기
+      </button>
+      {errMsgOn && (
+        <div className={ActivityCreateStyles.errMsg}>
+          <img
+            className={ActivityCreateStyles.warnIcon}
+            src={AlertTriangle}
+            alt="경고"
+          />
+          필수 입력값을 확인해주세요{' '}
+        </div>
+      )}
+      <Modal
+        isOpen={isBackModalOpen}
+        title={'이 페이지를 벗어나시겠어요?'}
+        content={'작성 내용이 초기화됩니다.'}
+        onClose={handleBackModal}
+        onConfirm={handleGoBack}
+        confirmText={'이동'}
+        cancleText={'취소'}
+      />
+
+      <Modal
+        isOpen={isCreateModalOpen}
+        title={'경험을 생성하시겠어요?'}
+        content={''}
+        onClose={handleCreateModal}
+        onConfirm={handleCreateActivity}
+        confirmText={'확인'}
+        cancleText={'취소'}
       />
     </>
   );

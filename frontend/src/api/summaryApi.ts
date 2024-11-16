e5ba57bae945dab262784b83d0e23a3514ba3987
hhsssu <hhssuu14@naver.com>
2024-11-16 21:16:08 +0900

    [FEAT] : 회고 수동 생성 / 수정 구현 완료

--- frontend/src/api/summaryApi.ts
+++ frontend/src/api/summaryApi.ts
@@ -1,10 +1,5 @@
 import axios from 'axios';
 import axiosInstance from './axiosInstance';
-// import useProjectStore from '../store/useProjectStore';
-
-// 프로젝트 ID 가져오기
-// TODO Invalid hook call 오류 발생하는 코드
-// const projectId = useProjectStore((state) => state.project.projectId);
 
 interface DiaryDetail {
   diaryId: number;
@@ -21,45 +16,40 @@ interface DiaryDetail {
 }
 
 // 회고 가져오기
-// export const fetchSummaryInfo = async () => {
-//   try {
-//     const response = await axiosInstance.get(`/projects/${projectId}/summary`);
-//     return response.data;
-//   } catch (error) {
-//     console.error('프로젝트 회고 가져오기 실패: ', error);
-//   }
-// };
+export const fetchSummaryInfo = async (projectId: number) => {
+  try {
+    const response = await axiosInstance.get(`/projects/${projectId}/summary`);
+    return response.data;
+  } catch (error) {
+    console.error('프로젝트 회고 가져오기 실패: ', error);
+  }
+};
 
-// // 회고 수동 생성
-// export const createSummary = async (content: string) => {
-//   try {
-//     const response = await axiosInstance.post(
-//       `/projects/${projectId}/summary`,
-//       {
-//         content: content,
-//       },
-//     );
-//     console.log('회고 수동 생성 성공: ', response.data);
-//   } catch (error) {
-//     console.error('프로젝트 회고 수동 생성 실패: ', error);
-//   }
-// };
+// 회고 수동 생성
+export const createSummary = async (projectId: number, content: string) => {
+  try {
+    await axiosInstance.post(`/projects/${projectId}/summary`, {
+      content: content,
+    });
+  } catch (error) {
+    console.error('프로젝트 회고 수동 생성 실패: ', error);
+  }
+};
 
-// TODO 회고 ID?
 // 회고 수정
-// export const updateSummary = async (content: string) => {
-// try {
-// const response = await axiosInstance.patch(
-//   `/projects/${projectId}/summary/${summaryId}`,
-//   {
-//     content: content,
-//   },
-// );
-// console.log('회고 수정 성공: ', response.data);
-//   } catch (error) {
-//     console.error('프로젝트 회고 수정 실패: ', error);
-//   }
-// };
+export const updateSummary = async (
+  projectId: number,
+  summaryId: number,
+  content: string,
+) => {
+  try {
+    await axiosInstance.patch(`/projects/${projectId}/summary/${summaryId}`, {
+      content: content,
+    });
+  } catch (error) {
+    console.error('프로젝트 회고 수정 실패: ', error);
+  }
+};
 
 // 회고 AI 생성
 export const createSummaryAi = async (projectId: number) => {

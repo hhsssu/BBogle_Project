e168285f834782792ab68b11f7672ab3f191a6d8
hhsssu <hhssuu14@naver.com>
2024-11-17 12:40:48 +0900

    [REFACTOR] : 경험 추출 버튼 이동

--- frontend/src/components/summary/SummaryDetail.tsx
+++ frontend/src/components/summary/SummaryDetail.tsx
@@ -1,5 +1,6 @@
 import Edit from '../../assets/image/icon/Pencil.svg';
 import styles from './Summary.module.css';
+import useProjectStore from '../../store/useProjectStore';
 
 interface SummaryDetailProps {
   onEditClick: () => void;
@@ -7,14 +8,30 @@ interface SummaryDetailProps {
 }
 
 function SummaryDetail({ onEditClick, content }: SummaryDetailProps) {
+  const project = useProjectStore((state) => state.project);
+
+  const requestCreateActivity = async () => {
+    // TODO 추후 연결 예정
+    // await createActivityAi();
+  };
+
   return (
-    <section className={styles.contentbox}>
-      <button className={styles.editbtn} onClick={onEditClick}>
-        <img src={Edit} alt="수정" />
-        <span className={styles.edit}>수정</span>
-      </button>
-      <div className={styles.content}>{content}</div>
-    </section>
+    <>
+      <section className={styles.contentbox}>
+        <button className={styles.editbtn} onClick={onEditClick}>
+          <img src={Edit} alt="수정" />
+          <span className={styles.edit}>수정</span>
+        </button>
+        <div className={styles.content}>{content}</div>
+      </section>
+      {!project.status && (
+        <div className={styles.center}>
+          <button className={styles.orangebtn} onClick={requestCreateActivity}>
+            경험 추출
+          </button>
+        </div>
+      )}
+    </>
   );
 }
 

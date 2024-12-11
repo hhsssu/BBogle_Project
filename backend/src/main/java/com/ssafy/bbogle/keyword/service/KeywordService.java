087ababbcc5741f09b352a3b5a9a2281144f24a0
문범수 <munbeumsu@hanmail.net>
2024-12-11 15:41:30 +0900

    Revert "[HOTFIX] : FE merge 누락 이슈 해결"
    This reverts commit f8ee90f264a594ac528270bcb39f7926d2809766

--- backend/src/main/java/com/ssafy/bbogle/keyword/service/KeywordService.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.bbogle.keyword.service;
-
-import com.ssafy.bbogle.keyword.dto.response.KeywordListResponse;
-
-public interface KeywordService {
-
-    KeywordListResponse getKeywordInfo();
-}

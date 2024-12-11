ff325ef714127adb82501fa2c20f60cfa8f25982
ANSMOON <munbeumsu@gmail.com>
2024-12-11 15:58:50 +0900

    Revert "[ALL] 합"
    This reverts commit eb9b5be19a59abea078ec6357b72067470923725.

--- backend/src/main/java/com/ssafy/bbogle/fcm/service/FcmService.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.bbogle.fcm.service;
-
-import com.ssafy.bbogle.fcm.dto.request.FcmSaveRequest;
-
-public interface FcmService {
-
-    void saveFcm(FcmSaveRequest request);
-}
--- frontend/src/api/alarmApi.ts
+++
@@ -1,8 +0,0 @@
-import axiosInstance from './axiosInstance';
-
-// 알림 구독을 위한 토큰 전송
-export const transferToken = async (token: string) => {
-  await axiosInstance.post('/fcm', {
-    fcmToken: token,
-  });
-};
